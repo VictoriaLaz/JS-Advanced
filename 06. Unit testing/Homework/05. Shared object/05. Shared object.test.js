b3f0ac2f939275bed850c60c797d75549ef401bb
Victoria Lazarova <lazarova.victoria@gmail.com>
2017-07-13 18:00:49 +0300

    ..

--- 06. Unit testing/Homework/05. Shared object/05. Shared object.test.js
+++ 06. Unit testing/Homework/05. Shared object/05. Shared object.test.js
@@ -10,30 +10,100 @@ document.body.innerHTML =
     `<div id="wrapper"><input type="text" id="name"><input type="text" id="income"></div>`;
 
 describe('shared object', function () {
-    let object;
-    beforeEach(function () {
-        object = sharedObject;
-    });
+
+
     describe('initial state', function () {
         it('name should return null', function () {
-            expect(object.name).to.be.equal(null);
+            expect(sharedObject.name).to.be.equal(null);
         });
         it('income should return null', function () {
-            expect(object.income).to.be.equal(null);
+            expect(sharedObject.income).to.be.equal(null);
         });
     });
     describe('change name', function () {
         it('changeName should not change the name property if an invalid value is passed, with preexisting value', function(){
-            object.name = "randomValue";
-            object.changeName('');
-            expect(object.name).to.be.equal("randomValue");
+            sharedObject.name = "randomValue";
+            sharedObject.changeName('');
+            expect(sharedObject.name).to.be.equal("randomValue");
         });
 
         it('changeName should not change the textbox value if an invalid value is passed, with preexisting value', function(){
             $('#name').val("Gosho");
-            object.changeName('');
+            sharedObject.changeName('');
             expect( $('#name').val()).to.be.equal("Gosho");
         });
+        it('changeName should change the name property if a valid value is passed, with preexisting value', function(){
+            sharedObject.name = "randomValue";
+            sharedObject.changeName('newRandomValue');
+            expect(sharedObject.name).to.be.equal("newRandomValue");
+        });
+        it('changeName should not change the textbox value if a valid value is passed, with preexisting value', function(){
+            $('#name').val("Gosho");
+            sharedObject.changeName('Stamat');
+            expect( $('#name').val()).to.be.equal("Stamat");
+        });
+    });
+    describe('changeIncome', function () {
+        it('changeIncome should not change the income property if an invalid value is passed, with preexisting value', function(){
+            sharedObject.income = 5;
+            sharedObject.changeIncome('Hello');
+            expect(sharedObject.income).to.be.equal(5);
+        });
+        it('changeIncome should not change the income property if an invalid value is passed, with preexisting value', function(){
+            sharedObject.income = 5;
+            sharedObject.changeIncome(-5);
+            expect(sharedObject.income).to.be.equal(5);
+        });
+        it('changeIncome should not change the income property if an invalid value is passed, with preexisting value', function(){
+            sharedObject.income = 5;
+            sharedObject.changeIncome(3.5);
+            expect(sharedObject.income).to.be.equal(5);
+        });
+        it('changeIncome should not change the income input if an invalid value is passed, with preexisting value', function(){
+            $('#income').val(5);
+            sharedObject.changeIncome(-5);
+            expect($('#income').val()).to.be.equal('5');
+        });
+        it('changeIncome should not change the income input if an invalid value is passed, with preexisting value', function(){
+            $('#income').val(5);
+            sharedObject.changeIncome('hello');
+            expect($('#income').val()).to.be.equal('5');
+        });
+        it('changeIncome should not change the income input if an invalid value is passed, with preexisting value', function(){
+            $('#income').val(5);
+            sharedObject.changeIncome(3.3);
+            expect($('#income').val()).to.be.equal('5');
+        });
+        it('changeIncome should change the income property if an invalid value is passed, with preexisting value', function(){
+            sharedObject.income = 5;
+            sharedObject.changeIncome(10);
+            expect(sharedObject.income).to.be.equal(10);
+        });
+        it('changeIncome should change the income input if an invalid value is passed, with preexisting value', function(){
+            $('#income').val(5);
+            sharedObject.changeIncome(10);
+            expect($('#income').val()).to.be.equal('10');
+        });
+    });
+    describe('updateName', function () {
 
+
+        it('updateName should not change the name property if an invalid value is passed, with preexisting value', function(){
+            $('#name').val('randomValue');
+            sharedObject.updateName('');
+            expect(sharedObject.name).to.be.equal("randomValue");
+        });
+
+
+        it('updateName should change the name property if a valid value is passed, with preexisting value', function(){
+            $('#name').val('newrandomValue');
+            sharedObject.updateName('newRandomValue');
+            expect(sharedObject.name).to.be.equal("newRandomValue");
+        });
+        it('updateName should not change the textbox value if a valid value is passed, with preexisting value', function(){
+            name = "Gosho";
+            sharedObject.updateName('Stamat');
+            expect(name).to.be.equal("Stamat");
+        });
     })
 });
